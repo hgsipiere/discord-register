55862c3631ff0d950329010222bed40881428c62
Hugh Sipiere <hgsipiere@gmail.com>
2020-09-15 18:03:28 +0100

    Use generic-lens instead to get rid of macros and underscores

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,13 @@
 module Main where
 
-import Calamity
+import Calamity hiding (author)
 import Calamity.Cache.InMemory
 import Calamity.Commands
 import Calamity.Metrics.Noop
 import qualified Calamity.HTTP.Guild
-import qualified Calamity.Types.Model.Channel.Message as M
+import Calamity.Types.Model.Channel.Message as M
 
-import Control.Lens ((^.), makeLenses)
+import Data.Generics.Internal.VL ((^.))
 import Control.Monad (void)
 
 import qualified Data.HashMap.Strict as Map (insert, empty, lookup, HashMap)
@@ -25,46 +25,40 @@ import qualified Polysemy as P
 import qualified Polysemy.AtomicState as P
 
 data Settings = Settings {
-  _botToken :: Text, _botID :: Snowflake User, _adminID :: Snowflake User, _vchannelID :: Snowflake Channel,
-  _joinMsg :: Text, _nameRecievedMsg :: Text,
-  _emailRecievedMsg :: Text, _noAtEmailMsg :: Text, _finishedMsg :: Text
+  botToken :: Text, botID :: Snowflake User, adminID :: Snowflake User, vchannelID :: Snowflake Channel,
+  joinMsg :: Text, nameRecievedMsg :: Text,
+  emailRecievedMsg :: Text, noAtEmailMsg :: Text, finishedMsg :: Text
 } deriving (Generic, Show)
-
-makeLenses ''Settings
 
 instance Dhall.FromDhall (Snowflake a)
 instance Dhall.FromDhall Settings
 
 data Form = Named Text | Finished
 
-emptyUserFormMap :: Map.HashMap (Snowflake User) Form
-emptyUserFormMap = Map.empty
-
 main :: IO ()
 main = do
-  cfg <- (Dhall.input Dhall.auto "./config.dhall" :: IO Settings)
+  cfg <- Dhall.input Dhall.auto "./config.dhall" :: IO Settings
   let program = do
-        react @'GuildMemberAddEvt $ \ctx -> do
-          void $ tell ctx $ cfg ^. joinMsg
+        react @'GuildMemberAddEvt \ctx -> void $ tell ctx $ cfg ^. #joinMsg
 
-        react @'MessageCreateEvt $ \ctx@M.Message{M.author=writer, M.guildID = guildIDOpt, M.content = response} -> do
+        react @'MessageCreateEvt \ctx@Message{author, M.content = response} ->
           -- the bot responding to its messages counts as an event, so ignore that to prevent looping
-          case (writer /= cfg ^. botID) && isNothing guildIDOpt of
+          case author /= cfg ^. #botID && isNothing (ctx ^. #guildID) of
             False -> pure ()
             True -> do
               current <- P.atomicGet
-              case Map.lookup writer current of
+              case Map.lookup author current of
                 Nothing -> do
-                  _ <- P.atomicPut $ Map.insert writer (Named response) current
-                  void $ tell writer $ cfg ^. nameRecievedMsg
+                  _ <- P.atomicPut $ Map.insert author (Named response) current
+                  void $ tell author $ cfg ^. #nameRecievedMsg
                 Just (Named name) -> case TL.foldl' (\t c -> t || c == '@') False response of
-                  False -> void $ tell writer $ cfg ^. noAtEmailMsg
+                  False -> void $ tell author $ cfg ^. #noAtEmailMsg
                   True -> do
-                    _ <- P.atomicPut (Map.insert writer Finished current)
-                    void $ tell (cfg ^. vchannelID) (name <> " " <> response)
-                    void $ tell writer $ cfg ^. emailRecievedMsg
-                Just Finished -> void $ tell writer $ cfg ^. finishedMsg
+                    _ <- P.atomicPut $ Map.insert author Finished current
+                    void $ tell (cfg ^. #vchannelID) (name <> " " <> response)
+                    void $ tell author $ cfg ^. #emailRecievedMsg
+                Just Finished -> void $ tell author $ cfg ^. #finishedMsg
   Di.new \di -> do
     void . P.runFinal . P.embedToFinal. runCacheInMemory . runDiToIO di. runMetricsNoop.
       -- if run without IO, the state is localised, ignore final state via fmap
-      fmap snd . P.atomicStateToIO emptyUserFormMap . runBotIO (BotToken $ cfg ^. botToken) $ program
+      fmap snd . P.atomicStateToIO Map.empty . runBotIO (BotToken $ cfg ^. #botToken) $ program
